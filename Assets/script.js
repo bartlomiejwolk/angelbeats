f03e097b65ed0f58068cd4bab57d2b6188e442a0
Bartłomiej Wołk <bartlomiejwolk@gmail.com>
2013-11-21 11:26:21 +0100

    Cleanup

--- Assets/script.js
+++ Assets/script.js
@@ -3,40 +3,34 @@ import System;
 import System.Text;
 import System.IO;
 
-var fileName = "angel_beats-hir_spaced.txt";
-
 function Start () {
+	// Source file
+	var fileName = "angel_beats-hir_spaced.txt";
 	var sr = StreamReader(Application.dataPath + "/" + fileName);
 	var fileContent = sr.ReadToEnd();
 	sr.Close();
+	
 	var sw : StreamWriter = new StreamWriter("converted.srt");
 	var output : String;
 	
-	var lines = fileContent.Split("\n"[0]);
-	// New StringBuilder?
-
+	var lines = fileContent.Split("\n"[0]);	
 		
-	for (line in lines) {
-		//line[0] = "x";
-		//Debug.Log(line);
-		// find line with timestamp [number, number, colon]
+	for (line in lines) {		
 		if (timeStamp(line)) {
 			// Delete all spaces except 7th and 8th						
 			var sb = new StringBuilder(line);
 			sb.Replace(" ", "", 0, 18);
 			sb.Replace(" ", "", 18, 15);					
 			
-			output += (sb.ToString() + "\n");
-			//output += (line + "\n");
-			Debug.Log(sb);			
-			//Debug.Log(line);
+			// Add line to output			
+			output += (sb.ToString() + "\n");			
 		}
 		else {
+			// Copy unchanged line
 			output += (line + "\n");;
 		}					
 	}
-	//Debug.Log(lines);
-	// save output to a new file
+	
 	sw.Write(output);
 	sw.Close();
 }
@@ -45,12 +39,12 @@ function Update () {
 
 }
 
+// Check if a given string is a TimeStamp
 function timeStamp(s : String) {
 	if (s[0] == '\r' || (s[0] != "0" || s[1] != "0")) {		
 		return false;
 	}
 	else {
 		return true;
-	}
-		
+	}	
 }
